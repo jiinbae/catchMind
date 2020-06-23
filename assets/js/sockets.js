55c374496d0249169b8b79946a57e43762544430
jiinbae <jiinbae98@gmail.com>
2020-06-24 01:16:56 +0900

    Connect and Disconnect Notifications

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,13 +1,14 @@
- import { handleNewUser } from "./notifications";
+import { handleNewUser, handleDisconnected } from "./notifications";
 
- let socket = null;
+let socket = null;
 
- export const getSocket = () => socket;
+export const getSocket = () => socket;
 
- export const updateSocket = aSocket => (socket = aSocket);
+export const updateSocket = aSocket => (socket = aSocket);
 
- export const initSockets = aSocket => {
+export const initSockets = aSocket => {
      const { events } = window;
      updateSocket(aSocket);
      aSocket.on(events.newUser, handleNewUser);
- };
+     aSocket.on(events.disconnected, handleDisconnected);
+};
