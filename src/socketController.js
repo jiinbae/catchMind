b13251ca7cd716840ebbc1afe4cc821f97776dce
jiinbae <jiinbae98@gmail.com>
2020-06-24 12:37:42 +0900

    Player Board part One

--- src/socketController.js
+++ src/socketController.js
@@ -1,24 +1,37 @@
 import events from "./events";
+import { setInterval } from "timers";
 
-const socketController = socket => {
+let sockets = [];
+
+const socketController = (socket, io) => {
     const broadcast = (event, data) => socket.broadcast.emit(event, data);
+    const superBroadcast = (event, data) => io.emit(event, data);
+    const sendPlayerUpdate = () =>
+        superBroadcast(events.playerUpdate, { sockets });
 
     socket.on(events.setNickname, ({ nickname }) => {
         socket.nickname = nickname;
+        socket.push({ id:socket.id, points: 0, nickname: nickname });
         broadcast(events.newUser, { nickname });
+        sendPlayerUpdate();
     });
 
     socket.on(events.disconnect, () => {
+        sockets =sockets.filter(aSocket.id !== socket.id);
         broadcast(events.disconnected, { nickname: socket.nickname });
+        sendPlayerUpdate();
     });
 
     socket.on(events.sendMsg, () => 
         broadcast(event.newMsg, { message, nickname: socket.nickname })
     );
 
+    socket.on(events.beginPath, ({ x, y }) =>
+        broadcast(events.beganPath, { x, y })
+    );
+
     socket.on(events.strokePath, ({ x, y, color }) => {
         broadcast(events.strokedPath, { x ,y, color });
-        console.log(x,y);
     });
 
     socket.on(events.fill, ({ color }) => {
