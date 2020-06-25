8ed0b3899bf562c0170cde064b382585dd4446f6
jiinbae <jiinbae98@gmail.com>
2020-06-25 11:15:58 +0900

    Game Programming part Three

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,7 +1,7 @@
 import { handleNewUser, handleDisconnected } from "./notifications";
 import { handleNewMessage } from "./chat";
 import { handleBeganPath, handleStrokedPath, handFilled } from "./paint";
-import { handlePlayerUpdate, handleGameStarted } from "./players";
+import { handlePlayerUpdate, handleGameStarted, handleLeaderNotif, handleGameEnded } from "./players";
 
 let socket = null;
 
@@ -18,4 +18,6 @@ export const initSockets = aSocket => {
      socket.on(events.filled, handFilled);
      socket.on(events.playerUpdate, handlePlayerUpdate);
      socket.on(events.gameStarted, handleGameStarted);
+     socket.on(events.leaderNotif, handleLeaderNotif);
+     socket.on(events.gameEnded, handleGameEnded);
 };
